a0e935387a0ae875813b9f793683e12eacf38ef2
weiwei.yang <weiwei.yang@zhangmen.com>
2021-06-18 14:28:14 +0800

    fix:up

--- app/task/main.js
+++ app/task/main.js
@@ -1,33 +1,29 @@
 const puppeteer = require('puppeteer');
 const cheerio = require("cheerio");
 const request = require('request')
+const rp  = require('request-promise');
 
-const open = async (browser, url) =>{
-    let page = await browser.newPage();
-    await page.goto(url);
-    await page.waitForTimeout(3000);
-
-    try{
-        const pageHtml = await page.$eval('.page-collection-detail-wrap .block-icon-list', (e) => e.outerHTML);
-        const groupText = await page.$eval('.block-sub-banner .right-content .title', (e) => e.outerText);
-        let $ = cheerio.load(pageHtml);
-        let data = []
-        $('li').each((index,obj) => {
-            data.push({
-                id:  $(obj).attr('class'),
-                type: "alibaba",
-                gurop: groupText,
-                CH_Name: $(obj).text(),
-                ENG_Name: "",
-                createTime: new Date(),
-                content: $(obj).find('.icon-twrap').html()
-            })
-        })
-        requestData(data, url)
-    }catch (error) { 
-        console.log('open url is error：', error)
+async function getIconName(id){
+    let rpbody = await rp({
+        uri: `https://www.iconfont.cn/api/icon/iconInfo.json?id=${id}`,
+        method:'GET',
+        headers: {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36",
+            'Content-Type': 'application/json'
+        }
+    });
+    let data = {}
+    try {
+        let body = JSON.parse(rpbody)
+        if(body.code == 200){
+            data = body.data
+        }else{
+            console.log("Get Icon name is error", body)
+        }
+    } catch (error){
+        console.log("Get Icon name is error: ", error, rpbody)
     }
-    await page.close()
+    return data
 }
 
 function requestData(data, url){
@@ -57,6 +53,43 @@ function requestData(data, url){
             console.log('添加失败', err)
         }
     })
+}
+
+const open = async (browser, url) =>{
+    let page = await browser.newPage();
+    await page.goto(url);
+    await page.waitForTimeout(3000);
+
+    try{
+        const pageHtml = await page.$eval('.page-collection-detail-wrap .block-icon-list', (e) => e.outerHTML);
+        const groupText = await page.$eval('.block-sub-banner .right-content .title', (e) => e.outerText);
+        let $ = cheerio.load(pageHtml);
+        let data = []
+
+        $('li').each(async (index,obj) => {
+            setTimeout(async ()=>{
+                let classNameId = $(obj).attr('class')
+                let id = classNameId.replace(/[^0-9]/ig,"")
+                let iconData = await getIconName(id)
+                console.log("index", index, iconData.font_class)
+                data.push({
+                    id: classNameId,
+                    type: "alibaba",
+                    gurop: groupText,
+                    CH_Name: $(obj).text(),
+                    ENG_Name: iconData.font_class || '',
+                    createTime: new Date(),
+                    content: $(obj).find('.icon-twrap').html()
+                })
+                if(index == $('li').length - 1){
+                    requestData(data, url)
+                }
+            }, 2200 * index + Math.ceil(Math.random()*1000))
+        })
+    }catch (error) { 
+        console.log('open url is error：', error)
+    }
+    await page.close()
 }
 
 (async () => {
