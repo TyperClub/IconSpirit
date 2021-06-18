1084f0f77b95279575ae943ce4c7a31025ab9a1f
weiwei.yang <weiwei.yang@zhangmen.com>
2021-06-18 21:00:44 +0800

    fix: up

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -5,7 +5,23 @@
         <navigation></navigation>
     </el-header>
     <el-main class="home">
-      
+        <div class="m-content">
+            <div class="m-row-user">
+                <div class="user-logo">
+                    <img src="https://sf6-ttcdn-tos.pstatp.com/img/user-avatar/693bcc26e2e1cb5cff0157182fd7f319~300x300.image" alt="">
+                </div>
+                <div class="user-info">
+                    <div>Hi,wiwi</div>
+                    <p>部门：效能研发部</p>
+                </div>
+            </div>
+            <el-tabs class="padding-top-20" v-model="activeName" @tab-click="handleClick">
+                <el-tab-pane label="我的项目" name="1">我的项目</el-tab-pane>
+                <el-tab-pane label="我的收藏" name="2">我的收藏</el-tab-pane>
+                <el-tab-pane label="我的部门" name="3">我的部门</el-tab-pane>
+            </el-tabs>
+        </div>
+   
     </el-main>
   </el-container>
 </el-container>
@@ -17,7 +33,7 @@ import Navigation from './Navigation';
   export default {
     data() {
       return {
-        activeIndex: '3',
+        activeName: "1",
       }
     },
     mounted(){
@@ -45,5 +61,51 @@ import Navigation from './Navigation';
 <style lang="less" scoped>
 .f-fr{
   float: right;
+}
+.home{
+    padding-top: 100px;
+}
+.padding-top-20{
+    padding-top: 20px;
+}
+.m-content{
+    position: relative;
+    min-height: calc(100vh - 64px);
+    max-width: 1240px;
+    padding: 0 20px;
+    margin: auto;
+}
+.m-row-user{
+    display: flex;
+    align-items: center;
+    color: #fff;
+    z-index: 2;
+    position: relative;
+    max-width: 1200px;
+    margin: 32px auto auto;
+    .user-logo{
+        margin-right: 25px;
+        width: 64px;
+        height: 64px;
+        border-radius: 50%;
+        overflow: hidden;
+        img{
+            width: 100%;
+            height: 100%;
+        }
+    }
+    .user-info{
+        div{
+            font-weight: 600;
+            color: #333;
+            font-size: 24px;
+            display: flex;
+            align-items: center;
+        }
+        p{
+            color: #999;
+            font-size: 14px;
+        }
+    }
 }
 </style>
