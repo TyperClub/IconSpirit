6149071d99a7f639f7e66d1bd12767dc14ac21b4
weiwei.yang <weiwei.yang@zhangmen.com>
2021-07-06 14:44:46 +0800

    fix:up

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,14 @@
 <template>
   <div id="app">
     <router-view class="router-view" v-slot="{ Component }">
-      <transition :name="transitionName">
         <component :is="Component" />
-      </transition>
     </router-view>
   </div>
 </template>
 
 <script>
-import { reactive, toRefs } from 'vue'
-import { useRouter } from 'vue-router'
   export default {
-    setup() {
-      const router = useRouter()
-      const state = reactive({
-        transitionName: 'slide-left'
-      })
-      router.beforeEach((to, from) => {
-        if (to.meta.index > from.meta.index) {
-          state.transitionName = 'slide-left' // 向左滑动
-        } else if (to.meta.index < from.meta.index) {
-          // 由次级到主级
-          state.transitionName = 'slide-right'
-        } else {
-          state.transitionName = ''   // 同级无过渡效果
-        }
-      })
-      return {
-        ...toRefs(state)
-      }
-    }
+
   }
 </script>
 
@@ -79,35 +57,6 @@ html, body {
     bottom: 0;
     margin: 0 auto;
     -webkit-overflow-scrolling: touch;
-}
-.slide-right-enter-active,
-.slide-right-leave-active,
-.slide-left-enter-active,
-.slide-left-leave-active{
-    height: 100%;
-    will-change: transform;
-    transition: all 500ms;
-    position: absolute;
-    backface-visibility: hidden;
-}
-.slide-right-enter{
-    opacity: 0;
-    transform: translate3d(-100%, 0, 0);
-}
-.slide-right-leave-active{
-    opacity: 0;
-    transform: translate3d(100%, 0, 0);
-}
-.slide-left-enter{
-    opacity: 0;
-    transform: translate3d(100%, 0, 0);
-}
-.slide-left-leave-active{
-    opacity: 0;
-    transform: translate3d(-100%, 0, 0);
-}
-.van-badge--fixed {
-  z-index: 1000;
 }
 .el-header {
     padding: 0;
