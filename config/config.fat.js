bb069b6401f51ee1185cd5a6f15e3074efe16d16
weiwei.yang <weiwei.yang@zhangmen.com>
2021-07-13 16:50:07 +0800

    fix:up

--- config/config.fat.js
+++ config/config.fat.js
@@ -4,6 +4,20 @@ module.exports = {
     cluster: {
         listen: {
             port: 8080,
-        },
+        }
+    },
+    mongoose: {
+        client: {
+            url: 'mongodb://127.0.0.1:27017/ops-iconfont',
+            options: {}
+        }
+    },
+    redis: {
+        client: {
+            port: 6379,          // Redis port
+            host: 'tars-redis.staging.zmaxis.com',   // Redis host
+            password: 'bC2kNyRH6f8kzEqpM2EV',
+            db: 5
+        }
     }
 };
