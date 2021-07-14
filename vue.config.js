64379ef2cbe873e775b44100d43d214b6f9ccaf6
weiwei.yang <weiwei.yang@zhangmen.com>
2021-07-14 19:30:14 +0800

    fix:up

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,11 @@
 const webpack = require("webpack");
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const config = require("./app/config");
 const Timestamp = new Date().getTime();
+const productionGzipExtensions = ['js', 'css']
 
 module.exports = {
+  productionSourceMap: false,
   devServer: {
     proxy: { // 配置跨域处理 可以设置多个
       '/api': {
@@ -19,13 +23,19 @@ module.exports = {
               $:"jquery",
               jQuery:"jquery",
               "windows.jQuery":"jquery"
-          })
-      ]
-  },
-  configureWebpack: { // webpack 配置
-    output: { // 输出重构  打包编译后的 文件名称  【模块名称.版本号.时间戳】
-      filename: `[name].${Timestamp}.js`,
-      chunkFilename: `[name].${Timestamp}.js`
-    },
+          }),
+          new CompressionWebpackPlugin({
+            filename: '[path].gz[query]',
+            algorithm: 'gzip',
+            test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),//匹配文件名
+            threshold: 10240,//对10K以上的数据进行压缩
+            minRatio: 0.8,
+            deleteOriginalAssets:false,//是否删除源文件
+        })
+      ],
+      output: { // 输出重构  打包编译后的 文件名称  【模块名称.版本号.时间戳】
+        filename: `[name].${Timestamp}.js`,
+        chunkFilename: `[name].${Timestamp}.js`
+      },
   }
 }
